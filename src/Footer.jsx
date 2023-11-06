ffa3cb9a850bb4b2c4ab48071b74ca1abf5205c8
Giannina <gftartaglia@gmail.com>
2023-11-06 15:22:51 -0300

    w

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer(){
     return(
       <footer>
-        <p>Made with ♥</p>
+        <p>Footer</p>
       </footer>
     )    
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
@@ -9,6 +10,19 @@ function Home() {
       <Footer />
     </>
   );
+=======
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Home(){
+    return(
+        <>
+        <Header></Header>
+        <p>Home</p>
+        <Footer></Footer>
+        </>
+    ) 
+>>>>>>> parent of 30e1a0e (added Components)
 }
 
 export default Home;
