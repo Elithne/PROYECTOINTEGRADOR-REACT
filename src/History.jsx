8344ba5f9947afde7b7c45c8d9ec57e5f8f899f3
Giannina <gftartaglia@gmail.com>
2023-11-06 15:26:42 -0300

    mistake

---
+++ src/History.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function History(){
+    return(
+        <>
+        <Header></Header>
+        <p>History</p>
+        <Footer></Footer>
+        </>
+    ) 
+}
+export default History;
