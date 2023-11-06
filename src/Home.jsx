ff06875e4fe65bae064d17e21b11c0c1d7970523
Giannina <gftartaglia@gmail.com>
2023-11-06 15:28:51 -0300

    mistake

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Home(){
+    return(
+        <>
+        <Header></Header>
+        <p>Home</p>
+        <Footer></Footer>
+        </>
+    ) 
+}
+export default Home;
