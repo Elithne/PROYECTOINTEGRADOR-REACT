02c93c51a408a60876b8a30b4e9bd1c539c1b130
Giannina <gftartaglia@gmail.com>
2023-11-06 19:44:27 -0300

    uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Home from './Home';
 import History from './History';
 import NotFound from './NotFound';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
 
 function App() {
   return (
